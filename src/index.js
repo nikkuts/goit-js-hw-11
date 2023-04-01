043b435dea4a4c9a62ff880f2cdfd64ee25b3cbd
nikkuts <nickkuts888@gmail.com>
2023-04-01 17:08:15 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import'./css/styles.css';
 import { Notify } from "../node_modules/notiflix/build/notiflix-notify-aio";
 import axios from "axios";
 
@@ -8,6 +9,7 @@ const ref = {
 };
 let searchQuery = '';
 let numberPage = 1;
+const perPage = 40;
 
 ref.form.addEventListener('submit', onSearch);
 ref.btnLoadMore.addEventListener('click', onLoadMore);
@@ -20,7 +22,7 @@ async function fetchFoto (query, number) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        per_page: 40,
+        per_page: perPage,
         page: number,
     });
     return await axios.get(`${BASE_URL}/?${params}`);
@@ -42,6 +44,7 @@ async function onSearch (event) {
         return;   
         }
         renderMarcup (data.hits);
+        ref.btnLoadMore.style.display = 'block';
     } 
     catch (error) {
         Notiflix.Notify.failure("ERROR Sorry, there are no images matching your search query. Please try again.");
@@ -61,44 +64,69 @@ function renderMarcup (images) {
     }) => {
         const marcup =
         `
-        <a href='${largeImageURL}'>
-            <div class="photo-card">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                <div class="info">
-                    <p class="info-item">
-                        <b>Likes</b>
-                        <span>${likes}</span>
-                    </p>
-                    <p class="info-item">
-                        <b>Views</b>
-                        <span>${views}</span>
-                    </p>
-                    <p class="info-item">
-                        <b>Comments</b>
-                        <span>${comments}</span>
-                    </p>
-                    <p class="info-item">
-                        <b>Downloads</b>
-                        <span>${downloads}</span>
-                    </p>
-                </div>
+        <div class="photo-card">
+            <a href='${largeImageURL}'>
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" width = 100% />
+            </a>
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b>
+                    <span>${likes}</span>
+                </p>
+                <p class="info-item">
+                    <b>Views</b>
+                    <span>${views}</span>
+                </p>
+                <p class="info-item">
+                    <b>Comments</b>
+                    <span>${comments}</span>
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b>
+                    <span>${downloads}</span>
+                </p>                    
             </div>
-        </a>
+        </div>
         `;
         return ref.gallery.insertAdjacentHTML('beforeend', marcup);
     });
 };
+
+// async function onLoadMore (event) {
+//     numberPage += 1; 
+
+//     try {
+//         const {data} = await fetchFoto (searchQuery, numberPage); console.log(data);
+//         // if (data.hits.length === 0) {
+//         //     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+//         //     return;   
+//         // }
+//         if (numberPage * perPage <= data.totalHits || data.hits.length !== 0) {
+//             renderMarcup (data.hits);
+//             ref.btnLoadMore.style.display = 'block'; return;   
+//         }
+//             ref.btnLoadMore.style.display = 'none';
+//             Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+//     } 
+//     catch (error) {
+//         Notiflix.Notify.failure("ERROR Sorry, there are no images matching your search query. Please try again.");
+//         console.log(error.message);        
+//     }    
+// };
 
 async function onLoadMore (event) {
     numberPage += 1; 
 
     try {
         const {data} = await fetchFoto (searchQuery, numberPage); console.log(data);
-        if (data.hits.length === 0) {
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        return;   
+
+        if (numberPage * perPage > data.totalHits) {
+            ref.btnLoadMore.style.display = 'none';
+            Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+            return;   
         }
-        renderMarcup (data.hits);
+            renderMarcup (data.hits);
+            ref.btnLoadMore.style.display = 'block';   
     } 
     catch (error) {
         Notiflix.Notify.failure("ERROR Sorry, there are no images matching your search query. Please try again.");
